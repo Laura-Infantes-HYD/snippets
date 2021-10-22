b5ff0353d319d9e6b4e29bb294255b9449d290da
Laura <laura@HYD-Admins-MacBook-Pro.local>
2021-10-22 11:29:19 +0200

    Adds authentication to get profile with access token

---
+++ snippets-api/middleware/authentication.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const authenticate = async (req, res, next) => {
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  const verification = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  if (!verification.id)
+    return res.status(401).json({ message: "Authentication error" });
+
+  let user;
+
+  try {
+    user = await User.findById(verification.id).select("-password");
+    if (user == null) {
+      return res.status(404).json({ message: "Cannot find user" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  delete user.password;
+
+  res.user = user;
+  next();
+};
+
+module.exports = authenticate;
